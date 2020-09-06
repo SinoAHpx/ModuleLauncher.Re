757528a9e44bb90389aecf5e086e9ecf571375f5
AHpxChina <AHpx@yandex.com>
2020-09-06 15:39:16 +0800

    Added OfflineAuthenticator.cs

---
+++ Authenticator/OfflineAuthenticator.cs
@@ -0,0 +1,19 @@
+﻿using ModuleLauncher.Re.Service.DataEntity.Authenticator;
+
+namespace ModuleLauncher.Re.Authenticator
+{
+    public class OfflineAuthenticator
+    {
+        public string Name { get; set; }
+
+        public OfflineAuthenticator(string name = "")
+        {
+            Name = name;
+        }
+
+        public AuthenticateResult Authenticate()
+        {
+            return Name;
+        }
+    }
+}
--- ModuleLauncher.Re.Test/Program.cs
+++ ModuleLauncher.Re.Test/Program.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using ModuleLauncher.Re.Authenticator;
 using ModuleLauncher.Re.Service.Utils;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ModuleLauncher.Re.Test
 {
@@ -10,7 +11,9 @@ namespace ModuleLauncher.Re.Test
     {
         public static void Main(string[] args)
         {
-            
+            var of = new OfflineAuthenticator("AHpx");
+            var rs = of.Authenticate();
+            Console.WriteLine(JObject.Parse(JsonConvert.SerializeObject(rs)));
         }
     }
 }
