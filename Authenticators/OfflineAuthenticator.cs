d841a337096697ae2aea9412a10e7ed15678786d
AHpx <ahpx@yandex.com>
2021-03-10 12:47:08 +0800

    Added constructor for OfflineAuthenticator.cs

--- Authenticators/OfflineAuthenticator.cs
+++ Authenticators/OfflineAuthenticator.cs
@@ -4,6 +4,11 @@ namespace AHpx.ModuleLauncher.Authenticators
 {
     public class OfflineAuthenticator
     {
+        public OfflineAuthenticator(string name = null)
+        {
+            Name = name;
+        }
+
         public string Name { get; set; }
 
         public AuthenticateResult Authenticate()
