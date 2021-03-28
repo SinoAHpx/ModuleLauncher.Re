f625aa573f3825a56369af41c4c6a72fef76f209
AHpx <ahpx@yandex.com>
2021-03-28 16:34:13 +0800

    Completed Downloader.cs & DownloadArgs.cs

--- Downloaders/Downloader.cs
+++ Downloaders/Downloader.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Reflection;
 using System.Threading.Tasks;
+using AHpx.ModuleLauncher.Data.Downloaders;
 using Downloader;
 using DownloadProgressChangedEventArgs = Downloader.DownloadProgressChangedEventArgs;
 
@@ -11,6 +12,65 @@ namespace AHpx.ModuleLauncher.Downloaders
 {
     public class Downloader
     {
+        private DownloadService _downloadService;
+
+        public Downloader()
+        {
+            _downloadService = new DownloadService(new DownloadConfiguration
+            {
+                ParallelDownload = true,
+                RequestConfiguration = new RequestConfiguration
+                {
+                    AllowAutoRedirect = true,
+                    UserAgent = "ModuleLauncher/2.8"
+                }
+            });
+
+            _downloadService.DownloadStarted += new EventHandler<DownloadStartedEventArgs>(DownloadStared);
+            _downloadService.DownloadFileCompleted += new EventHandler<AsyncCompletedEventArgs>(DownloadCompleted);
+            _downloadService.DownloadProgressChanged += new EventHandler<DownloadProgressChangedEventArgs>(DownloadProgressChanged);
+        }
+
+        public Action<DownloadArgs.StartedArgs> StartedAction { get; set; } = args => { };
+        public Action<DownloadArgs.CompletedArgs> CompletedAction { get; set; } = args => { };
+        public Action<DownloadArgs.ProgressArgs> ProgressAction { get; set; } = args => { };
         
+        public async Task Download(string address, string fileName)
+        {
+            await _downloadService.DownloadFileTaskAsync(address, fileName);
+        }
+
+        private void DownloadStared(object sender, DownloadStartedEventArgs e)
+        {
+            StartedAction?.Invoke(new DownloadArgs.StartedArgs
+            {
+                FileName = e.FileName,
+                FileSize = e.TotalBytesToReceive
+            });
+        }
+        
+        private void DownloadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            CompletedAction?.Invoke(new DownloadArgs.CompletedArgs
+            {
+                Cancelled = e.Cancelled,
+                Exception = e.Error
+            });
+        }
+        
+        private void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            ProgressAction?.Invoke(new DownloadArgs.ProgressArgs
+            {
+                AverageBytesPerSecondSpeed = e.AverageBytesPerSecondSpeed,
+                BytesPerSecondSpeed = e.BytesPerSecondSpeed,
+                Percentage = e.ProgressPercentage,
+                ProgressedBytesSize = e.ProgressedByteSize,
+                ProgressID = e.ProgressId,
+                ReceivedBytes = e.ReceivedBytes,
+                ReceivedBytesSize = e.ReceivedBytesSize,
+                TotalBytesSize = e.TotalBytesToReceive
+            });
+        }
     }
 }
--- Entrance.cs
+++ Entrance.cs
@@ -10,6 +10,8 @@ using AHpx.ModuleLauncher.Downloaders;
 using AHpx.ModuleLauncher.Locators;
 using AHpx.ModuleLauncher.Utils.Extensions;
 using AHpx.ModuleLauncher.Utils.Network;
+using Downloader;
+using MoreLinq;
 
 namespace AHpx.ModuleLauncher
 {
@@ -17,23 +19,99 @@ namespace AHpx.ModuleLauncher
     {
         public static async Task Main(string[] args)
         {
-            var ms = new MicrosoftAuthenticator("M.R3_BAY.208b1c40-517e-7865-1b72-9d6eaa017fbd");
-            // var re = await ms.Authenticate();
-            //
-            // Console.WriteLine(re.Name);
-            // Console.WriteLine(re.Uuid);
-            // Console.WriteLine(re.AccessToken);
-            // Console.WriteLine(re.Verified);
+            var downloader = new Downloaders.Downloader();
 
-            Console.WriteLine(await ms.CheckHaveMinecraft("eyJhbGciOiJIUzI1NiJ9.eyJ4dWlkIjoiMjUzNTQ0MDM0MjkwNzc3NyIsInN1YiI6IjY4ZGNjMzA1LTIyNTItNDMzZS1iYzQ5LWE2ODI3NGMzOTliYyIsIm5iZiI6MTYxNDg2NzQ2OSwicm9sZXMiOltdLCJpc3MiOiJhdXRoZW50aWNhdGlvbiIsImV4cCI6MTYxNDk1Mzg2OSwiaWF0IjoxNjE0ODY3NDY5fQ.cLNnisLHr0kO8w1vI79t49EBt5CNMxCRKs2HMuiXXd0"));
+            downloader.StartedAction += startedArgs =>
+            {
+                Console.WriteLine($"Download {startedArgs.FileName} stared!");
+            };
+            downloader.CompletedAction += completedArgs =>
+            {
+                Console.WriteLine("Download completed!");
+            };
+            downloader.ProgressAction += progressArgs =>
+            {
+                Console.WriteLine(
+                    $"The current downloading progress is {progressArgs.ReceivedBytesSize}/{progressArgs.TotalBytesSize} bytes");
+            };
+
+            await downloader.Download(
+                "https://launcher.mojang.com/v1/objects/07ad8b23e33d195fd897133c9c0e35d7fa1593eb/client.jar",
+                @"C:\Users\ahpx\Desktop\Test\Test.jar");
+
+            // var fileNames = new string[]
+            // {
+            //     "Test_File_1",
+            //     "Test_File_2",
+            //     "Test_File_3",
+            //     "AHpx_File_1",
+            //     "AHpx_File_2",
+            //     "AHpx_File_3",
+            //     "Shit_File_1",
+            //     "Shit_File_2",
+            //     "Shit_File_3",
+            //     "Single_File_1",
+            // };
+            //
+            // await downloadParallel(fileNames);
+            //
+            // Console.WriteLine("Download completed!");
+            // //await Task.WhenAll(t1("awd"), t1("awd1"), t1("awd2"));
         }
 
+        private static async Task downloadParallel(string[] fileNames, int maxCount = 3)
+        {
+            var name = fileNames.Batch(maxCount);
+            var tasks = new List<Task>();
+            foreach (var enumerable in name)
+            {
+                foreach (var s in enumerable)
+                {
+                    tasks.Add(t1(s));
+                }
+                await Task.WhenAll(tasks);
+                tasks.Clear();
+            }   
+        }
+        
+        private static async Task t1(string name)
+        {
+            var loc = @"C:\Users\ahpx\Desktop\Test\";
+            
+            var download = new DownloadService(new DownloadConfiguration
+            {
+                ParallelDownload = true
+            });
+
+            download.DownloadStarted += (sender, eventArgs) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"This download {Path.GetFileName(eventArgs.FileName)} is started!!!");
+            };
+            download.DownloadFileCompleted += (sender, eventArgs) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"This download is finished!!!");
+            };
+            download.DownloadProgressChanged += (sender, eventArgs) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(
+                    $"The current downloading progress is {eventArgs.ReceivedBytesSize}/{eventArgs.TotalBytesToReceive} bytes");
+            };
+            
+            await download.DownloadFileTaskAsync("http://ovh.net/files/1Mio.dat", $"{loc}{name}.txt");
+        }
+        
         private static void Output<T>(this IEnumerable<T> ex)
         {
+            Console.Write("[");
             foreach (var x1 in ex)
             {
-                Console.WriteLine(x1);
+                Console.Write($"{x1}, ");
             }
+
+            Console.WriteLine("]");
         }
     }
 }
