f3fe8020b4aa08f6d4775eb562bfc96e229d35e1
AHpx <ahpx@yandex.com>
2021-04-10 20:45:11 +0800

    removed

--- Data/Downloaders/Minecraft/LatestMinecrafts.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AHpx.ModuleLauncher.Data.Downloaders.Minecraft
-{
-    public class LatestMinecrafts
-    {
-        public string Release { get; set; }
-        public string Snapshot { get; set; }
-    }
-}
