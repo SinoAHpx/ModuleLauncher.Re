c2f4dd0d36d6b2f3d85f8a2b96cdb0ee49e76b0a
AHpx <AHpx@yandex.com>
2021-06-14 01:10:56 +0800

    Accomplished MojangAuthenticator.cs

---
+++ ModuleLauncher.Re/Authenticators/MojangAuthenticator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using ModuleLauncher.Re.Models.Authenticators;
+using ModuleLauncher.Re.Utils;
+using ModuleLauncher.Re.Utils.Extensions;
+
+namespace ModuleLauncher.Re.Authenticators
+{
+    public class MojangAuthenticator : AuthenticatorBase
+    {
+        private string _account;
+        private string _password;
+        private string _clientToken;
+
+        public string Account
+        {
+            get => _account;
+            set => _account = value ?? throw new ArgumentNullException($"{nameof(Account)} can't be null!");
+        }
+
+        public string Password
+        {
+            get => _password;
+            set => _password = value ?? throw new ArgumentNullException($"{nameof(Password)} can't be null!");
+        }
+
+        public string ClientToken
+        {
+            get => _clientToken;
+            set => _clientToken = value ?? Guid.NewGuid().ToString("N");
+        }
+
+        public MojangAuthenticator()
+        {
+            ClientToken = null;
+        }
+
+        public MojangAuthenticator(string account = null, string password = null)
+        {
+            _account = account;
+            _password = password;
+        }
+
+        public MojangAuthenticator(string account, string password, string clientToken = null)
+        {
+            _account = account;
+            _password = password;
+            _clientToken = clientToken;
+        }
+
+        public override async Task<AuthenticateResult> Authenticate()
+        {
+            var url = "https://authserver.mojang.com/authenticate";
+            var payload = new
+            {
+                agent = new
+                {
+                    name = "Minecraft",
+                    version = 1
+                },
+                username = Account,
+                password = Password,
+                clientToken = ClientToken,
+                requestUser = true
+            }.ToJsonString();
+
+            var result = await HttpUtility.PostJson(url, payload);
+
+            if (!IsSuccess(result))
+            {
+                throw GetException(result);
+            }
+
+            return result.Content.ToJsonEntity<AuthenticateResult>();
+        }
+    }
+}
--- ModuleLauncher.Test/Program.cs
+++ ModuleLauncher.Test/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using ModuleLauncher.Re.Authenticators;
+using ModuleLauncher.Re.Utils.Extensions;
+using Newtonsoft.Json.Linq;
 
 namespace ModuleLauncher.Test
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var obj = JObject.Parse(File.ReadAllText(@"C:\Users\ahpx\Desktop\account.json"));
+            
+            var ac = new MojangAuthenticator
+            {
+                Account = obj.Fetch("Account"),
+                Password = "lwidkjmawd",
+                ClientToken = null
+            };
+
+            var re = await ac.Authenticate();
+
+            Console.WriteLine(re.ToJsonString());
         }
     }
 }
