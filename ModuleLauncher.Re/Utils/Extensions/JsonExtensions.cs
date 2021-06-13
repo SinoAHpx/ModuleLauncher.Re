57440cbca37279d499c91a351409e88b26e67a05
AHpx <AHpx@yandex.com>
2021-06-13 23:56:15 +0800

    Add extensions

---
+++ ModuleLauncher.Re/Utils/Extensions/JsonExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace ModuleLauncher.Re.Utils.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static string ToJsonString(this object obj, bool ignoreNull = true)
+        {
+            return JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings
+            {
+                NullValueHandling = ignoreNull ?  NullValueHandling.Ignore :  NullValueHandling.Include
+            });
+        }
+    }
+}
---
+++ ModuleLauncher.Re/Utils/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ModuleLauncher.Re.Utils.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+    }
+}
