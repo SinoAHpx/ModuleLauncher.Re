d3f99f4ded144d75a1984b598cf53d36b917941b
AHpx <ahpx@yandex.com>
2021-06-14 01:10:35 +0800

    Add MojangAuthenticator.cs

---
+++ ModuleLauncher.Re/Authenticators/AuthenticatorBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using ModuleLauncher.Re.Models.Authenticators;
+using ModuleLauncher.Re.Utils.Extensions;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace ModuleLauncher.Re.Authenticators
+{
+    public abstract class AuthenticatorBase
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string ClientToken { get; set; }
+        
+        public abstract Task<AuthenticateResult> Authenticate();
+
+        internal Exception GetException(IRestResponse response)
+        {
+            var obj = JObject.Parse(response.Content);
+                
+            return new Exception($"Error: {obj.Fetch("error")}\r\nMessage: {obj.Fetch("errorMessage")}");
+        }
+
+        internal bool IsSuccess(IRestResponse response)
+        {
+            return response.StatusCode == HttpStatusCode.OK;
+        }
+    }
+}
---
+++ ModuleLauncher.Re/Models/Authenticators/AuthenticateResult.cs
@@ -0,0 +1,24 @@
+﻿using ModuleLauncher.Re.Utils.Extensions;
+using Newtonsoft.Json;
+
+namespace ModuleLauncher.Re.Models.Authenticators
+{
+    [JsonConverter(typeof(JsonPathConverter))]
+    public class AuthenticateResult
+    {
+        [JsonProperty("user.username")]
+        public string Account { get; set; }
+        
+        [JsonProperty("selectedProfile.name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("clientToken")]
+        public string ClientToken { get; set; }
+        
+        [JsonProperty("accessToken")]
+        public string AccessToken { get; set; }
+        
+        [JsonProperty("selectedProfile.id")]
+        public string Uuid { get; set; }
+    }
+}
