0778b81688e8ad3c6b383fc336d3101faf73a1cf
AHpx <AHpx@yandex.com>
2021-06-13 23:56:28 +0800

    Add HttpUtility.cs

---
+++ ModuleLauncher.Re/Utils/HttpUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace ModuleLauncher.Re.Utils
+{
+    public static class HttpUtility
+    {
+        private static RestClient _client;
+        
+        static HttpUtility()
+        {
+            _client = new RestClient();
+        }
+
+        public static async Task<string> PostJson(string url, string json)
+        {
+            _client.BaseUrl = new Uri(url);
+
+            var request = new RestRequest(Method.POST);
+            request.AddJsonBody(json);
+
+            return (await _client.ExecuteAsync(request)).Content;
+        }
+    }
+}
