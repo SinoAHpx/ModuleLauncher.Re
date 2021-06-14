b52be87342f91a35c39e0baffcd63ea6f090f54f
AHpx <AHpx@yandex.com>
2021-06-15 00:49:20 +0800

    Add MojangAuthenticatorExtensions.cs

---
+++ ModuleLauncher.Re/Utils/Extensions/MojangAuthenticatorExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using ModuleLauncher.Re.Authenticators;
+using ModuleLauncher.Re.Models.Authenticators;
+using Newtonsoft.Json.Linq;
+
+namespace ModuleLauncher.Re.Utils.Extensions
+{
+    public static class MojangAuthenticatorExtensions
+    {
+        private static MojangAuthenticator _mojangAuthenticator = new MojangAuthenticator();
+        
+        public static async Task Invalidate(this AuthenticateResult result)
+        {
+            await _mojangAuthenticator.Invalidate(result.AccessToken, result.ClientToken);
+        }
+        
+        public static async Task<bool> Validate(this AuthenticateResult authenticateResult, bool throwException = false)
+        {
+            return await _mojangAuthenticator.Validate(authenticateResult.AccessToken, authenticateResult.ClientToken,
+                throwException);
+        }
+        
+        public static async Task<AuthenticateResult> Refresh(this AuthenticateResult authenticateResult)
+        {
+            return await _mojangAuthenticator.Refresh(authenticateResult.AccessToken, authenticateResult.ClientToken);
+        }
+    }
+}
