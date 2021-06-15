22d8dbab88f2b128965e16eba9ea264fe7387430
AHpx <AHpx@yandex.com>
2021-06-15 23:58:50 +0800

    Add summary

--- ModuleLauncher.Re/Utils/Extensions/MojangAuthenticatorExtensions.cs
+++ ModuleLauncher.Re/Utils/Extensions/MojangAuthenticatorExtensions.cs
@@ -11,17 +11,30 @@ namespace ModuleLauncher.Re.Utils.Extensions
     {
         private static MojangAuthenticator _mojangAuthenticator = new MojangAuthenticator();
         
+        /// <summary>
+        /// Invalidate a access token from MojangAuthenticator
+        /// </summary>
+        /// <param name="result"></param>
         public static async Task Invalidate(this AuthenticateResult result)
         {
             await _mojangAuthenticator.Invalidate(result.AccessToken, result.ClientToken);
         }
-        
+
+        /// <summary>
+        /// Validate a access token from MojangAuthenticator
+        /// </summary>
+        /// <param name="authenticateResult"></param>
+        /// <param name="throwException">Throw when exception occured</param>
         public static async Task<bool> Validate(this AuthenticateResult authenticateResult, bool throwException = false)
         {
             return await _mojangAuthenticator.Validate(authenticateResult.AccessToken, authenticateResult.ClientToken,
                 throwException);
         }
         
+        /// <summary>
+        /// Refresh a access token from MojangAuthenticator
+        /// </summary>
+        /// <param name="authenticateResult"></param>
         public static async Task<AuthenticateResult> Refresh(this AuthenticateResult authenticateResult)
         {
             return await _mojangAuthenticator.Refresh(authenticateResult.AccessToken, authenticateResult.ClientToken);
