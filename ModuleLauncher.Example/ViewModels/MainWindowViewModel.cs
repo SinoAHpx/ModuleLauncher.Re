1d30e5b913658eb5b76836fe0ea9279315fd9aa5
AHpx <AHpx@yandex.com>
2021-06-16 22:41:46 +0800

    Add Avalonia-based example project

---
+++ ModuleLauncher.Example/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Dialogs;
+using ModuleLauncher.Example.Views;
+
+namespace ModuleLauncher.Example.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+       
+    }
+}
