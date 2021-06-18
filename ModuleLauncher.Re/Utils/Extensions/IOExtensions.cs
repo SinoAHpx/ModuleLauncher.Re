0c2f8e3f374939cfbb8ba76d35e8f64ce81ddd0c
AHpx <AHpx@yandex.com>
2021-06-18 22:13:03 +0800

    Add IOExtensions.cs

---
+++ ModuleLauncher.Re/Utils/Extensions/IOExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace ModuleLauncher.Re.Utils.Extensions
+{
+    public static class IOExtensions
+    {
+        /// <summary>
+        /// Convert a directory path string to DirectoryInfo
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static DirectoryInfo ToDirectoryInfo(this string s)
+        {
+            return new DirectoryInfo(s);
+        }
+
+        /// <summary>
+        /// Convert a file path string to FileInfo
+        /// </summary>
+        /// <returns></returns>
+        public static FileInfo ToFileInfo(this string s)
+        {
+            return new FileInfo(s);
+        }
+    }
+}
