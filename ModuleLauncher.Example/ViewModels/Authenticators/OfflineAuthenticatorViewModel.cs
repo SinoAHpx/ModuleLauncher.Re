ca21fc03a9faa95befaab23691e87c4c9abe65cc
AHpx <AHpx@yandex.com>
2021-06-18 21:27:41 +0800

    Add OfflineAuthenticatorView.axaml

---
+++ ModuleLauncher.Example/ViewModels/Authenticators/OfflineAuthenticatorViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ModuleLauncher.Example.Extensions;
+using ModuleLauncher.Re.Authenticators;
+using ModuleLauncher.Re.Utils.Extensions;
+using ReactiveUI;
+
+namespace ModuleLauncher.Example.ViewModels.Authenticators
+{
+    public class OfflineAuthenticatorViewModel : ViewModelBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+        
+        public async void Authenticate()
+        {
+            await MessageBoxEx.Show((await new OfflineAuthenticator(Name).Authenticate()).ToJsonString());
+        }
+    }
+}
---
+++ ModuleLauncher.Example/Views/Authenticators/OfflineAuthenticatorView.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using ModuleLauncher.Example.ViewModels.Authenticators;
+
+namespace ModuleLauncher.Example.Views.Authenticators
+{
+    public class OfflineAuthenticatorView : UserControl
+    {
+        public OfflineAuthenticatorView()
+        {
+            InitializeComponent();
+
+            DataContext = new OfflineAuthenticatorViewModel();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
