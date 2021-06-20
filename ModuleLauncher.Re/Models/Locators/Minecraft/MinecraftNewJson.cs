56876b3fdc8c3790b1c2e5f5a5ba4db6f45660c6
AHpx <AHpx@yandex.com>
2021-06-21 01:09:50 +0800

    Add Minecraft jsons

---
+++ ModuleLauncher.Re/Models/Locators/Minecraft/MinecraftNewJson.cs
@@ -0,0 +1,19 @@
+﻿using ModuleLauncher.Re.Utils.Extensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ModuleLauncher.Re.Models.Locators.Minecraft
+{
+    [JsonConverter(typeof(JsonPathConverter))]
+    public class MinecraftNewJson : MinecraftCommonJson
+    {
+        [JsonProperty("assets")]
+        public string AssetId { get; set; }
+        
+        [JsonProperty("assetIndex.url")]
+        public string AssetIndexUrl { get; set; }
+        
+        [JsonProperty("arguments")]
+        public JToken MinecraftArguments { get; set; }
+    }
+}
---
+++ ModuleLauncher.Re/Models/Locators/Minecraft/MinecraftOldJson.cs
@@ -0,0 +1,21 @@
+﻿using ModuleLauncher.Re.Utils.Extensions;
+using Newtonsoft.Json;
+
+namespace ModuleLauncher.Re.Models.Locators.Minecraft
+{
+    /// <summary>
+    /// 1.7.10 -> 1.12.2
+    /// </summary>
+    [JsonConverter(typeof(JsonPathConverter))]
+    public class MinecraftOldJson : MinecraftCommonJson
+    {
+        [JsonProperty("assets")]
+        public string AssetId { get; set; }
+        
+        [JsonProperty("assetIndex.url")]
+        public string AssetIndexUrl { get; set; }
+        
+        [JsonProperty("minecraftArguments")]
+        public string MinecraftArguments { get; set; }
+    }
+}
