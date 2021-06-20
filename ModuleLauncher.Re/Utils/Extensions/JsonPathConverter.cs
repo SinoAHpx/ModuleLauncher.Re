9e77dd244734ca00b19bf8472c18adb895c54033
AHpx <AHpx@yandex.com>
2021-06-21 00:42:52 +0800

    Enhance JsonPathConverter.cs

--- ModuleLauncher.Re/Utils/Extensions/JsonPathConverter.cs
+++ ModuleLauncher.Re/Utils/Extensions/JsonPathConverter.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Reflection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -15,24 +14,23 @@ namespace ModuleLauncher.Re.Utils.Extensions
         public override object ReadJson(JsonReader reader, Type objectType, 
             object existingValue, JsonSerializer serializer)
         {
-            JObject jo = JObject.Load(reader);
-            object targetObj = Activator.CreateInstance(objectType);
+            var jo = JObject.Load(reader);
+            var targetObj = Activator.CreateInstance(objectType);
 
-            foreach (PropertyInfo prop in objectType.GetProperties()
+            foreach (var prop in objectType.GetProperties()
                 .Where(p => p.CanRead && p.CanWrite))
             {
-                JsonPropertyAttribute att = prop.GetCustomAttributes(true)
+                var att = prop.GetCustomAttributes(true)
                     .OfType<JsonPropertyAttribute>()
                     .FirstOrDefault();
 
-                string jsonPath = (att != null ? att.PropertyName : prop.Name);
-                JToken token = jo.SelectToken(jsonPath);
+                var jsonPath = (att != null ? att.PropertyName : prop.Name);
+                var token = jo.SelectToken(jsonPath!);
 
-                if (token != null && token.Type != JTokenType.Null)
-                {
-                    object value = token.ToObject(prop.PropertyType, serializer);
-                    prop.SetValue(targetObj, value, null);
-                }
+                if (token == null || token.Type == JTokenType.Null) continue;
+                
+                var value = token.ToObject(prop.PropertyType, serializer);
+                prop.SetValue(targetObj, value, null);
             }
 
             return targetObj;
@@ -44,10 +42,7 @@ namespace ModuleLauncher.Re.Utils.Extensions
             return false;
         }
 
-        public override bool CanWrite
-        {
-            get { return false; }
-        }
+        public override bool CanWrite => false;
 
         public override void WriteJson(JsonWriter writer, object value,
             JsonSerializer serializer)
