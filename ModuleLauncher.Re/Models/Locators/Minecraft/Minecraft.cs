a691efa16fad68fafc3651cf1618619c2b694bbb
AHpx <AHpx@yandex.com>
2021-06-21 01:46:08 +0800

    Add Minecraft.cs

---
+++ ModuleLauncher.Re/Models/Locators/Minecraft/Minecraft.cs
@@ -0,0 +1,9 @@
+﻿namespace ModuleLauncher.Re.Models.Locators.Minecraft
+{
+    public class Minecraft
+    {
+        public LocalVersion Locality { get; set; }
+
+        public MinecraftJson Json { get; set; }
+    }
+}
---
+++ ModuleLauncher.Re/Models/Locators/Minecraft/MinecraftJson.cs
@@ -0,0 +1,37 @@
+﻿using ModuleLauncher.Re.Utils.Extensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ModuleLauncher.Re.Models.Locators.Minecraft
+{
+    [JsonConverter(typeof(JsonPathConverter))]
+    public class MinecraftJson
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        
+        [JsonProperty("libraries")]
+        public JArray Libraries { get; set; }
+        
+        [JsonProperty("mainClass")]
+        public string MainClass { get; set; }
+        
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        
+        [JsonProperty("assets")]
+        public string AssetId { get; set; }
+        
+        [JsonProperty("assetIndex.url")]
+        public string AssetIndexUrl { get; set; }
+        
+        [JsonProperty("arguments")]
+        public JToken Arguments { get; set; }
+
+        [JsonProperty("minecraftArguments")]
+        public string MinecraftArguments { get; set; }
+
+        [JsonProperty("inheritsFrom")]
+        public string InheritsFrom { get; set; }
+    }
+}
