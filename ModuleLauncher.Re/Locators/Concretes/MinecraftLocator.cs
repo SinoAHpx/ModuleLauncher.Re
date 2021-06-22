d82b015b19ac53e5b7fa42162b74282972110a2f
AHpx <AHpx@yandex.com>
2021-06-23 00:22:20 +0800

    Fix xml-doc syntax error

--- ModuleLauncher.Re/Locators/Concretes/MinecraftLocator.cs
+++ ModuleLauncher.Re/Locators/Concretes/MinecraftLocator.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using ModuleLauncher.Re.Models.Locators.Minecraft;
 using ModuleLauncher.Re.Utils.Extensions;
 
@@ -8,6 +11,10 @@ namespace ModuleLauncher.Re.Locators.Concretes
     {
         private readonly LocalityLocator _locator;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="locator">A string can be implicitly converted to LocalityLocator</param>
         public MinecraftLocator(LocalityLocator locator)
         {
             _locator = locator;
@@ -27,6 +34,11 @@ namespace ModuleLauncher.Re.Locators.Concretes
             
             foreach (var version in versions)
             {
+                if (!version.Json.Exists)
+                {
+                    throw new IOException($"No json file exist for {version.Version.Name}!");
+                }
+                
                 var json = version.Json.ReadAllText();
                 var entity = json.ToJsonEntity<MinecraftJson>();
 
@@ -40,6 +52,25 @@ namespace ModuleLauncher.Re.Locators.Concretes
             }
 
             return re;
+        }
+
+        /// <summary>
+        /// Get single local minecraft via specified minecraft id
+        /// </summary>
+        /// <param name="id">e.g. 1.8.9</param>
+        /// <returns></returns>
+        public Minecraft GetLocalMinecraft(string id)
+        {
+            try
+            {
+                var minecrafts = GetLocalMinecrafts();
+
+                return minecrafts.First(x => x.Raw.Id == id);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("The specified Minecraft was not found!", e);
+            }
         }
     }
 }
