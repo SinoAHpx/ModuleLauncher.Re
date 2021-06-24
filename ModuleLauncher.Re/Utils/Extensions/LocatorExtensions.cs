32401321839d7c30924165d5198f7c8c3edb7f88
AHpx <AHpx@yandex.com>
2021-06-25 00:02:55 +0800

    Add LocatorExtensions.cs

---
+++ ModuleLauncher.Re/Utils/Extensions/LocatorExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ModuleLauncher.Re.Locators.Concretes;
+using ModuleLauncher.Re.Locators.Dependencies;
+using ModuleLauncher.Re.Models.Locators.Dependencies;
+
+namespace ModuleLauncher.Re.Utils.Extensions
+{
+    static class LocatorExtensions
+    {
+        /// <summary>
+        /// Extension GetLibraries method for MinecraftLocator via local minecraft id
+        /// Equals to librariesLocator.GetDependencies(mc)
+        /// </summary>
+        /// <param name="minecraftLocator"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        internal static async Task<IEnumerable<Dependency>> GetLibraries(this MinecraftLocator minecraftLocator, string id)
+        {
+            var librariesLocator = new LibrariesLocator(minecraftLocator);
+            var mc = await minecraftLocator.GetLocalMinecraft(id);
+
+            return await librariesLocator.GetDependencies(mc);
+        }
+
+        /// <summary>
+        /// Extension GetNatives method for MinecraftLocator via local minecraft id
+        /// </summary>
+        /// <param name="minecraftLocator"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        internal static async Task<IEnumerable<Dependency>> GetNatives(this MinecraftLocator minecraftLocator, string id)
+        {
+            var librariesLocator = new LibrariesLocator(minecraftLocator);
+            var mc = await minecraftLocator.GetLocalMinecraft(id);
+
+            return await librariesLocator.GetNativeDependencies(mc); 
+        }
+    }
+}
