77902dbc45bbf61ad29d30694f52afb054c36ab2
AHpx <AHpx@yandex.com>
2021-06-26 00:52:35 +0800

    Add DownloaderSource.cs

---
+++ ModuleLauncher.Re/Models/Downloaders/DownloaderSource.cs
@@ -0,0 +1,9 @@
+﻿namespace ModuleLauncher.Re.Models.Downloaders
+{
+    public enum DownloaderSource
+    {
+        Mojang,
+        Bmclapi,
+        Mcbbs
+    }
+}
---
+++ ModuleLauncher.Re/Utils/Extensions/EnumerateExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using ModuleLauncher.Re.Models.Downloaders;
+
+namespace ModuleLauncher.Re.Utils.Extensions
+{
+    public static class EnumerateExtensions
+    {
+
+    }
+}
