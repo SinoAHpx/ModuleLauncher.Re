5fc99a1dd02ba1ad0db1803cc56e6f46defd5c00
AHpx <AHpx@yandex.com>
2021-06-26 01:25:32 +0800

    Add GetMinecrafts methods

--- ModuleLauncher.Re/Downloaders/MinecraftDownloader.cs
+++ ModuleLauncher.Re/Downloaders/MinecraftDownloader.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using ModuleLauncher.Re.Models.Downloaders;
+using ModuleLauncher.Re.Models.Downloaders.Minecraft;
 using ModuleLauncher.Re.Utils;
 using ModuleLauncher.Re.Utils.Extensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ModuleLauncher.Re.Downloaders
 {
@@ -46,6 +50,38 @@ namespace ModuleLauncher.Re.Downloaders
             var re = (manifest.Fetch("latest.release"), manifest.Fetch("latest.snapshot"));
 
             return re;
+        }
+
+        /// <summary>
+        /// Get remote minecrafts collections
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<MinecraftDownloadItem>> GetRemoteMinecrafts()
+        {
+            var manifest = await FetchManifest();
+            var versions = manifest.Fetch("versions").ToObject<JArray>();
+            var re = new List<MinecraftDownloadItem>();
+            
+            foreach (var token in versions!)
+            {
+                var item = JsonConvert.DeserializeObject<MinecraftDownloadItem>(token.ToString());
+
+                re.Add(item);
+            }
+            
+            return re;
+        }
+
+        /// <summary>
+        /// Get single minecraft via minecraft id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<MinecraftDownloadItem> GetRemoteMinecraft(string id)
+        {
+            var minecrafts = await GetRemoteMinecrafts();
+
+            return minecrafts.First(x => x.Id == id);
         }
     }
 }
--- ModuleLauncher.Test/Program.cs
+++ ModuleLauncher.Test/Program.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Downloader;
 using ModuleLauncher.Re.Downloaders;
 using ModuleLauncher.Re.Locators.Concretes;
+using ModuleLauncher.Re.Models.Downloaders.Minecraft;
+using ModuleLauncher.Re.Utils.Extensions;
 
 namespace ModuleLauncher.Test
 {
@@ -15,10 +18,12 @@ namespace ModuleLauncher.Test
         {
             var foo = new MinecraftDownloader();
 
-            var bar = await foo.GetLatestVersions();
-
-            Console.WriteLine(bar.Item1);
-            Console.WriteLine(bar.Item2);
+            var bar = await foo.GetRemoteMinecrafts();
+            
+            foreach (var item in bar.Where(x => x.Type == MinecraftDownloadType.OldAlpha))
+            {
+                Console.WriteLine(item.ToJsonString());
+            }
         }
     }
 }
