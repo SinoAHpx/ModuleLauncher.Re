3488874cd283cf49b9e945847dbd7be1bf81fb6d
AHpx <AHpx@yandex.com>
2021-07-04 00:44:45 +0800

    Add DownloadUtility.cs

---
+++ ModuleLauncher.Re/Utils/DownloadUtility.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq.Expressions;
+using System.Net;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml;
+using Downloader;
+using Polly;
+using DownloadProgressChangedEventArgs = Downloader.DownloadProgressChangedEventArgs;
+
+namespace ModuleLauncher.Re.Utils
+{
+    /// <summary>
+    /// Download utilities
+    /// </summary>
+    internal class DownloadUtility
+    {
+        private readonly string UserAgent = $"ModuleLauncher.Re/{SystemUtility.GetAssemblyVersion()}";
+        
+        internal Action<DownloadStartedEventArgs> DownloadStarted { get; set; }
+
+        internal Action<AsyncCompletedEventArgs> DownloadCompleted { get; set; }
+
+        internal Action<DownloadProgressChangedEventArgs> DownloadProgressChanged { get; set; }
+
+        public Action<Exception, int> OnRetry { get; set; }
+
+        internal (string url, FileInfo file) DownloadInfo { get; set; }
+
+        private DownloadService _service;
+
+        internal async Task Download()
+        {
+            var configuration = new DownloadConfiguration
+            {
+                ParallelDownload = true,
+                MaxTryAgainOnFailover = 114514,
+                OnTheFlyDownload = true,
+                RequestConfiguration = new RequestConfiguration
+                {
+                    UserAgent = UserAgent
+                }
+            };
+
+            var policy = Policy
+                .Handle<Exception>()
+                .RetryAsync(3, (exception, i) =>
+                {
+                    OnRetry?.Invoke(exception, i);
+                });
+
+            try
+            {
+                await policy.ExecuteAsync(async () =>
+                {
+                    using (_service = new DownloadService(configuration))
+                    {
+                        _service.DownloadStarted += (sender, args) => { DownloadStarted?.Invoke(args); };
+                        _service.DownloadProgressChanged += (sender, args) =>
+                        {
+                            DownloadProgressChanged?.Invoke(args);
+                        };
+                        _service.DownloadFileCompleted += (sender, args) => { DownloadCompleted?.Invoke(args); };
+
+                        await _service.DownloadFileTaskAsync(DownloadInfo.url, DownloadInfo.file.FullName).ConfigureAwait(false);
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                throw new WebException("Download failed!", e);
+            }
+        }
+
+        public void Cancel()
+        {
+            _service.CancelAsync();
+        }
+    }
+}
--- ModuleLauncher.Test/Program.cs
+++ ModuleLauncher.Test/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Downloader;
 using ModuleLauncher.Re.Authenticators;
@@ -23,71 +24,7 @@ namespace ModuleLauncher.Test
         //java executable file: 
         static async Task Main(string[] args)
         {
-
-            var list1 = new List<string>()
-            {
-                "aa",
-                "bb",
-                "cc"
-            };
-
-            var list2 = new List<string>(list1);
-
-            list2.Add("dd");
-
-            list1.ForEach(Console.WriteLine);
-
-            // const string root = @"C:\Users\ahpx\AppData\Roaming\.minecraft";
-            // const string ver = "1.15.2";
-            //
-            // var librariesLocator = new LibrariesLocator(root);
-            //
-            // foreach (var dependency in await librariesLocator.GetDependencies(ver))
-            // {
-            //     Console.WriteLine(dependency.Name);
-            // }
-            // try
-            // {
-            //     var minecraftDownloader = new MinecraftDownloader(root);
-            //
-            //     minecraftDownloader.DownloadStarted += DownloadStarted;
-            //     minecraftDownloader.DownloadCompleted += DownloadCompleted;
-            //     minecraftDownloader.DownloadProgressChanged += DownloadProgressChanged;
-            //
-            //     await minecraftDownloader.Download(ver);
-            // }
-            // catch (Exception e)
-            // {
-            //     Console.WriteLine(e.Message);
-            // }
-            //
-            // var librariesLocator = new LibrariesLocator(root);
-            // var assetsLocator = new AssetsLocator(root);
-            //
-            // var dependencies = await librariesLocator.GetDependencies(ver);
-            //
-            // dependencies = dependencies.Union(await assetsLocator.GetDependencies(ver));
-            //
-            // var dependenciesDownloader = new DependenciesDownloader(dependencies);
-            //
-            // dependenciesDownloader.DownloadStarted += DownloadStarted;
-            // dependenciesDownloader.DownloadCompleted += DownloadCompleted;
-            // dependenciesDownloader.DownloadProgressChanged += DownloadProgressChanged;
-            //
-            // await dependenciesDownloader.Download(32);
-            //
-            // var launcher = new Launcher(root)
-            // {
-            //     Java = @"C:\Program Files\Java\jre1.8.0_291\bin\javaw.exe",
-            //     Authentication = "AHpx"
-            // };
-            //
-            // var process = await launcher.Launch(ver);
-            //
-            // while (await process.StandardOutput.ReadLineAsync() != null)
-            // {
-            //     Console.WriteLine(await process.StandardOutput.ReadLineAsync());
-            // }
+            
         }
 
         private static void DownloadStarted(DownloadStartedEventArgs e)
