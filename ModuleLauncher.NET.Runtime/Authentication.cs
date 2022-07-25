47624a54860c2c304eed32a7f5fff0c38d8932ba
AHpx <ahpx@yandex.com>
2022-07-25 18:37:48 +0800

    Re-stored authentication testing codes

---
+++ ModuleLauncher.NET.Runtime/Authentication.cs
@@ -0,0 +1,30 @@
+﻿using Manganese.Text;
+using ModuleLauncher.NET.Authentication;
+using ModuleLauncher.NET.Utilities;
+using Spectre.Console;
+
+namespace ModuleLauncher.NET.Runtime;
+
+public class Authentication
+{
+    public async Task DoAuthAsync()
+    {
+        var code = AnsiConsole.Ask<string>("What is your [red]code[/]? ");
+        var authenticator = new MicrosoftAuthenticator
+        {
+            ClientId = "1b0c7f80-247c-4101-bdc9-a98c479471a4",
+            Code = code.ExtractCode()
+        };
+
+        var result = await authenticator.AuthenticateAsync();
+
+        AnsiConsole.MarkupLine(result.ToJsonString().EscapeMarkup());
+
+        AnsiConsole.MarkupLine("[red]refreshing...[/]");
+
+        var refreshToken = result.RefreshToken;
+        var newResult = await authenticator.RefreshAuthenticateAsync(refreshToken);
+
+        AnsiConsole.MarkupLine($"new result: {newResult.ToJsonString()}");
+    }
+}
--- ModuleLauncher.NET.Runtime/Program.cs
+++ ModuleLauncher.NET.Runtime/Program.cs
@@ -7,20 +7,3 @@ using ModuleLauncher.NET.Utilities;
 using Newtonsoft.Json.Linq;
 using Spectre.Console;
 
-var code = AnsiConsole.Ask<string>("What is your [red]code[/]? ");
-var authenticator = new MicrosoftAuthenticator
-{
-    ClientId = "1b0c7f80-247c-4101-bdc9-a98c479471a4",
-    Code = code.ExtractCode()
-};
-
-var result = await authenticator.AuthenticateAsync();
-
-AnsiConsole.MarkupLine(result.ToJsonString().EscapeMarkup());
-
-AnsiConsole.MarkupLine("[red]refreshing...[/]");
-
-var refreshToken = result.RefreshToken;
-var newResult = await authenticator.RefreshAuthenticateAsync(refreshToken);
-
-AnsiConsole.MarkupLine($"new result: {newResult.ToJsonString()}");
