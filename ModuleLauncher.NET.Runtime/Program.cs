844abb8bc310e3396db1c3d7e79a57cce0f02513
AHpx <ahpx@yandex.com>
2022-07-25 18:41:51 +0800

    Added global using file

--- ModuleLauncher.NET.Runtime/Program.cs
+++ ModuleLauncher.NET.Runtime/Program.cs
@@ -1,9 +1,1 @@
-﻿using Flurl.Http;
-using Manganese.Text;
-using ModuleLauncher.NET.Authentication;
-using ModuleLauncher.NET.Models.Authentication;
-using ModuleLauncher.NET.Runtime;
-using ModuleLauncher.NET.Utilities;
-using Newtonsoft.Json.Linq;
-using Spectre.Console;
-
+﻿AnsiConsole.MarkupLine("wad");
