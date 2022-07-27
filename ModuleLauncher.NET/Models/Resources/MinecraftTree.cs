2d819634b860784cce5023c930c16f0137b3e803
AHpx <ahpx@yandex.com>
2022-07-27 21:07:18 +0800

    Cancel nullable

--- ModuleLauncher.NET/Models/Resources/MinecraftTree.cs
+++ ModuleLauncher.NET/Models/Resources/MinecraftTree.cs
@@ -13,61 +13,61 @@ public class MinecraftTree
     /// <summary>
     /// .minecraft/versions
     /// </summary>
-    public DirectoryInfo? Versions { get; set; }
+    public DirectoryInfo Versions { get; set; }
 
     /// <summary>
     /// .minecraft/saves
     /// </summary>
-    public DirectoryInfo? Saves { get; set; }
+    public DirectoryInfo Saves { get; set; }
 
     /// <summary>
     /// .minecraft/mods
     /// </summary>
-    public DirectoryInfo? Mods { get; set; }
+    public DirectoryInfo Mods { get; set; }
 
     /// <summary>
     /// .minecraft/resourcepacks
     /// </summary>
-    public DirectoryInfo? ResourcesPacks { get; set; }
+    public DirectoryInfo ResourcesPacks { get; set; }
 
     /// <summary>
     /// .minecraft/texturepacks
     /// <remarks>Only for old version minecraft</remarks>
     /// </summary>
-    public DirectoryInfo? TexturePacks { get; set; }
+    public DirectoryInfo TexturePacks { get; set; }
 
     /// <summary>
     /// .minecraft/libraries
     /// </summary>
-    public DirectoryInfo? Libraries { get; set; }
+    public DirectoryInfo Libraries { get; set; }
 
     /// <summary>
     /// .minecraft/assets/objects
     /// </summary>
-    public DirectoryInfo? Assets { get; set; }
+    public DirectoryInfo Assets { get; set; }
 
     /// <summary>
     /// .minecraft/assets/indexes
     /// </summary>
-    public DirectoryInfo? AssetsIndexes { get; set; }
+    public DirectoryInfo AssetsIndexes { get; set; }
 
     /// <summary>
     /// .minecraft/versions/%ver%/%ver%.jar
     /// </summary>
-    public FileInfo? Jar { get; set; }
+    public FileInfo Jar { get; set; }
 
     /// <summary>
     /// .minecraft/versions/%ver%/%ver%.json
     /// </summary>
-    public FileInfo? Json { get; set; }
+    public FileInfo Json { get; set; }
 
     /// <summary>
     /// .minecraft/versions/%ver%
     /// </summary>
-    public DirectoryInfo? Version { get; set; }
+    public DirectoryInfo Version { get; set; }
 
     /// <summary>
     /// .minecraft/versions/%ver%/natives
     /// </summary>
-    public DirectoryInfo? Natives { get; set; }
+    public DirectoryInfo Natives { get; set; }
 }
