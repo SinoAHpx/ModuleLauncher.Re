f92f281ef982acd08576babd59aef31f31201a28
AHpx <ahpx@yandex.com>
2022-07-30 22:00:51 +0800

    Added logo asset

--- ModuleLauncher.NET.Runtime/Program.cs
+++ ModuleLauncher.NET.Runtime/Program.cs
@@ -1,12 +1,4 @@
-﻿using System.Text;
-using Flurl.Http;
-using Manganese.Array;
-using Manganese.Text;
-using ModuleLauncher.NET.Models.Utils;
-using ModuleLauncher.NET.Runtime;
-using ModuleLauncher.NET.Utilities;
-using Newtonsoft.Json;
-
+﻿AnsiConsole.MarkupLine("markup");
 
 static class RuntimeUtils
 {
