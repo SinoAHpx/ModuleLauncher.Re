ae3642724a7f7026bc4094e534561370878e5c63
AHpx <ahpx@yandex.com>
2022-08-01 15:09:46 +0800

    Accomplished UI skeleton

---
+++ ModuleLauncher.NET.Example/Views/UtilsView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ModuleLauncher.NET.Example.Views;
+
+public partial class UtilsView : UserControl
+{
+    public UtilsView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
