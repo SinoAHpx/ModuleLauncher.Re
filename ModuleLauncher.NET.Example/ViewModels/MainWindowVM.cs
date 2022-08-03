04bd425be6895d74764acded7f9f38512292a461
AHpxLin <ahpx@yandex.com>
2022-08-03 09:12:19 +0800

    Removed test codes

--- ModuleLauncher.NET.Example/ViewModels/MainWindowVM.cs
+++ ModuleLauncher.NET.Example/ViewModels/MainWindowVM.cs
@@ -5,31 +5,5 @@ namespace ModuleLauncher.NET.Example.ViewModels
 {
     public class MainWindowVM : ViewModelBase
     {
-        public ObservableCollection<Node> Items { get; }
-        public ObservableCollection<Node> SelectedItems { get; }
-
-        public MainWindowVM()
-        {
-            var dir = new DirectoryInfo("/home/ahpx/Documents/Minecrafts/.minecraft");
-
-            Items = new ObservableCollection<Node>();
-
-            var rootNode = new Node(dir);
-            rootNode.SubNodes.Add(new Node(dir.Dive("versions")));
-
-            Items.Add(rootNode);
-        }
-
-        public class Node
-        {
-            public ObservableCollection<Node> SubNodes { get; set; } = new();
-
-            public DirectoryInfo Dir { get; set; }
-
-            public Node(DirectoryInfo dir)
-            {
-                Dir = dir;
-            }
-        }
     }
 }
