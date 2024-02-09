288f1f3bcf8bd38e8761530eb227443ecfae1eca
AHpx <ahpx@yandex.com>
2024-02-09 21:47:53 +0800

    Upgraded dotnet version

--- ModuleLauncher.NET.Runtime/Program.cs
+++ ModuleLauncher.NET.Runtime/Program.cs
@@ -1,21 +1,30 @@
 ﻿using Manganese.Text;
 using ModuleLauncher.NET.Mods.Utilities;
+using ModuleLauncher.NET.Resources;
+using ModuleLauncher.NET.Utilities;
+using System.Runtime.CompilerServices;
 using Tommy;
 
-while (true)
-{
-    var info = await ModUtils.GetModInfoAsync(AnsiConsole.Ask<string>("Mod [red]path[/]:").Trim('"'));
-    info.ToJsonString().Print();
-}
+const string MinecraftRoot = @"C:\Users\ahpx\AppData\Roaming\.minecraft";
+var resolver = new MinecraftResolver(MinecraftRoot);
 
-    
+var mc = resolver.GetMinecraft("1.20.4");
+
+await mc.WithAuthentication("AHpx")
+    .WithJava(@"C:\Users\ahpx\AppData\Local\Packages\Microsoft.4297127D64EC6_8wekyb3d8bbwe\LocalCache\Local\runtime\java-runtime-gamma\windows-x64\java-runtime-gamma\bin\javaw.exe")
+    .WithLauncherName("Latest Version")
+    .LaunchAsync(pipeTarget: CliWrap.PipeTarget.ToDelegate(s =>
+    {
+        Console.WriteLine(s);
+    }));
+
 static class RuntimeUtils
 {
 
     public static T Print<T>(this T t)
     {
         Console.WriteLine(t);
-        
+
         return t;
     }
 }
