6fee111d0b689d66ad01dfd3c71b6d8252a4272a
AHpx-Lap <ahpx@yandex.com>
2024-07-01 21:06:45 +0800

    Updated dependencies

--- ModuleLauncher.NET.Runtime/Program.cs
+++ ModuleLauncher.NET.Runtime/Program.cs
@@ -9,10 +9,10 @@ using Tommy;
 const string MinecraftRoot = @"C:\Users\ahpx\AppData\Roaming\.minecraft";
 var resolver = new MinecraftResolver(MinecraftRoot);
 
-var mc = resolver.GetMinecraft("fabric-loader-0.15.11-1.21");
+var mc = resolver.GetMinecraft("1.8.9");
 
 var process = await mc.WithAuthentication("AHpx")
-    .WithJava(@"C:\Program Files\Eclipse Adoptium\jre-21.0.3.9-hotspot\bin\javaw.exe")
+    .WithJava(@"C:\Program Files\Eclipse Adoptium\jre-8.0.412.8-hotspot\bin\javaw.exe")
     .WithLauncherName("Latest Version")
     .WithDirectServer("hypixel.net")
     .LaunchAsync();
